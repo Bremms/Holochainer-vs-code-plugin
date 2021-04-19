11a5db005a048d78b741d11276bc8241cc94b7d2
Arne De Bremme <di_code@outlook.com>
2021-04-19 23:31:12 +0200

    POC commands nix-shell & nixDefaultFile

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
+const terminal = (show: boolean = true): vscode.Terminal => {
+	let terminal = vscode.window.activeTerminal == null ? vscode.window.createTerminal() : vscode.window.activeTerminal;
 
+	if (show)
+		terminal.show();
+
+	return terminal;
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,15 +21,52 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('holochainer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Holochainer!');
+	let nixShell = vscode.commands.registerCommand('holochainer.nix', () => {
+		terminal().sendText("nix-shell .");
 	});
+	let createDefNix = vscode.commands.registerCommand("holochainer.defaultNix", () => {
+		const wsedit = new vscode.WorkspaceEdit();
+		if(vscode.workspace.workspaceFolders == undefined){
+			vscode.window.showInformationMessage('Open a workspace to create the default nix file');
+			return;
+		}
 
-	context.subscriptions.push(disposable);
+		const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
+		const filePath = vscode.Uri.file(wsPath + '/default.nix');
+	
+		wsedit.createFile(filePath, { ignoreIfExists: true });
+		var encoder = new TextEncoder()
+		vscode.workspace.fs.writeFile(filePath,encoder.encode(defaultNixFileContent));
+		
+		
+		vscode.workspace.applyEdit(wsedit);
+		vscode.window.showInformationMessage('Created a new file: default.nix');
+		
+	});
+	context.subscriptions.push(nixShell);
+	context.subscriptions.push(createDefNix);
 }
-
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+//Replace this with a file if I find a way to target the src files.
+
+let defaultNixFileContent = `let
+holonixPath = builtins.fetchTarball {
+  url = "https://github.com/holochain/holonix/archive/90a19d5771c069dbcc9b27938009486b54b12fb7.tar.gz";
+  sha256 = "11wv7mwliqj38jh1gda3gd0ad0vqz1d42hxnhjmqdp037gcd2cjg";
+};
+holonix = import (holonixPath) {
+  includeHolochainBinaries = true;
+  holochainVersionId = "custom";
+
+  holochainVersion = {
+   rev = "d3a61446acaa64b1732bc0ead5880fbc5f8e3f31";
+   sha256 = "0k1fsxg60spx65hhxqa99nkiz34w3qw2q4wspzik1vwpkhk4pwqv";
+   cargoSha256 = "0fz7ymyk7g3jk4lv1zh6gbn00ad7wsyma5r7csa88myl5xd14y68";
+   bins = {
+	 holochain = "holochain";
+	 hc = "hc";
+   };
+  };
+};
+in holonix.main`
